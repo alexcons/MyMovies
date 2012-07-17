d4ec83caa7e8787990d25c699cfa3f3d9d8e5e6b
Alejandro Cons <alexcons@gmail.com>
2012-07-17 09:57:25 -0500

    Added Splashscreen
    Added Stopwatch to measure performance

---
+++ Konz.MyMovies.UI/Source/AppStopWatch.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+public static class AppStopWatch
+{
+    private static List<KeyValuePair<string, TimeSpan>> times = new List<KeyValuePair<string, TimeSpan>>();
+    private static Stopwatch watch = new Stopwatch();
+
+    internal static void Start()
+    {
+        times.Clear();
+        watch.Reset();
+        watch.Start();
+    }
+
+    internal static void Stop()
+    {
+        watch.Stop();
+    }
+
+    internal static void Lap(string name)
+    {
+        times.Add(new KeyValuePair<string, TimeSpan>(name, watch.Elapsed));
+    }
+
+    internal static string GetLaps()
+    {
+        return string.Join("\n", (from t in times select t.Key + "=" + decimal.Round((decimal)t.Value.TotalMilliseconds, 0).ToString()).ToArray());
+    }
+}
